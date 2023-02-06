c2506b4fc1d6dc3ef908dde494b66fd0b3b04057
ThomG1 <thomas.gwyther@hotmail.co.uk>
2023-02-06 14:16:26 +0000

    add quotes

--- .env.py
+++ .env.py
@@ -1,3 +1,3 @@
 import os
 
-os.environ.setdefault("MONGO_URI", mongodb+srv://tdgcode:Morply30@myfirstcluster.wihixx0.mongodb.net/?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://tdgcode:Morply30@myfirstcluster.wihixx0.mongodb.net/?retryWrites=true&w=majority")
